8ae796b71c6be88ff197d596a5b7acbbf1f27c62
Kate0418 <nakao.keito0418@gmail.com>
2026-04-12 01:14:05 +0900

    ドラックアンドドロップ完了

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./global.css";
 import Page from "./page";
 
 createRoot(document.getElementById("root")!).render(
